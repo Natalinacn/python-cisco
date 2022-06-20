f6fcbf4d1d39a6cfa53ebfca65ba3514bf399e81
Natalinacn <natalinacuna@gmail.com>
2022-06-20 01:19:54 -0300

    Agregué ejercicios Módulo 3

---
+++ 3.2.1.15 hipótesisDeCollatz_While.py
@@ -0,0 +1,26 @@
+#Objetivos
+#Familiarizar al estudiante con:
+#Utilizar el bucle while.
+#Convertir bucles definidos verbalmente en código real de Python.
+
+# En 1937, un matemático alemán llamado Lothar Collatz formuló una hipótesis intrigante
+# (aún no se ha comprobado) que se puede describir de la siguiente manera:
+
+# Toma cualquier número entero que no sea negativo y que no sea cero y asígnale el nombre c0.
+# Si es par, evalúa un nuevo c0 como c0 ÷ 2.
+# De lo contrario, si es impar, evalúe un nuevo  c0  como 3 × c0 + 1.
+# Si c0 ≠ 1, salta al punto 2.
+# La hipótesis dice que, independientemente del valor inicial de c0, el valor siempre tiende a 1.
+
+
+c0 = int(input("Ingresa un número positivo: "))
+pasos = 0
+while c0 > 1:
+    if c0 % 2 == 0:
+        c0 = c0 / 2
+    else: 
+        c0 = (3* c0) + 1
+    pasos+=1
+    print(c0)
+print("La cantidad de pasos es de ", pasos)
+
