bf4e75ed72262a6eb5573b5aebb284255d1647ca
Natalinacn <natalinacuna@gmail.com>
2022-06-27 18:23:08 -0300

    Ejercicios módulo 3

---
+++ 3.4.1.6 loBásicoDeLasListas.py
@@ -0,0 +1,36 @@
+'''
+Objetivos
+Familiarizar al estudiante con:
+
+Usar instrucciones básicas relacionadas con listas.
+Crear y modificar listas.
+Escenario
+Había una vez un sombrero. El sombrero no contenía conejo, sino una lista de cinco números: 1, 2, 3, 4 y 5.
+
+Tu tarea es:
+
+Escribir una línea de código que solicite al usuario que reemplace el número central en la lista con un número entero ingresado por el usuario (Paso 1).
+Escribir una línea de código que elimine el último elemento de la lista (Paso 2).
+Escribir una línea de código que imprima la longitud de la lista existente (Paso 3).
+¿Listo para este desafío?
+
+'''
+
+hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+# Paso 1: escribe una línea de código que solicite al usuario
+# reemplazar el número de en medio con un número entero ingresado por el usuario.
+
+numero=int(input("Ingrese un número para reemplazar el número del medio: "))
+hat_list[2]=numero
+
+# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+
+del hat_list[-1]
+
+
+# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+
+print(len(hat_list))
+print(hat_list)
+
